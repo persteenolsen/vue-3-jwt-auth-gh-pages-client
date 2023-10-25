17607b9fd301ea3db3dc0db3c2423d8eb07ae6a5
Per Steen Olsen <persteen22@hotmail.com>
2023-10-25 12:04:22 +0200

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,20 @@
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
+
+import App from './App.vue';
+import { router } from './helpers';
+
+// setup fake backend
+//VITE_API_URL=http://localhost:4000
+
+// setup real backend
+//VITE_API_URL=https://pso-aspnet-core-jwt.azurewebsites.net/
+//import { fakeBackend } from './helpers';
+//fakeBackend();
+
+const app = createApp(App);
+
+app.use(createPinia());
+app.use(router);
+
+app.mount('#app');
